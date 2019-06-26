271e37c66b019850949ef15cdcbe8f80648b7790
Kaptan Evgeniy <evgeniykaptan@gmail.com>
2019-06-26 22:12:29 +0300

    sql file

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
 	login VARCHAR(32) NOT NULL,
 	email VARCHAR(320),
 	-- id изображения аватарки
-	avatar_id INTEGER(64) UNSIGNED,
+	avatar INTEGER(64) UNSIGNED,
 	-- SHA1 хэш (40 символов)
 	password_hash VARCHAR(40),
 	-- UUID (36 символов)
@@ -14,10 +14,17 @@ CREATE TABLE users (
 	rights ENUM('user', 'administrator') DEFAULT 'user',
 
 	-- Информация о записи в базе
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER users_insert_created
+	BEFORE INSERT ON users 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE login_attempts (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -27,20 +34,33 @@ CREATE TABLE login_attempts (
 	ip VARCHAR(40),
 	user_agent TEXT,
 
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP)
+	created INTEGER(64)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER login_attempt_created
+	BEFORE INSERT ON login_attempts 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE sessions (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
-	user_id INTEGER(64) UNSIGNED,
-	-- 36 символов UUID
+	user INTEGER(64) UNSIGNED,
+	
 	token VARCHAR(36),
 
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER session_created
+	BEFORE INSERT ON sessions 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE tweets (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -50,59 +70,95 @@ CREATE TABLE tweets (
 	text VARCHAR(280),
 	image_id INTEGER(64) UNSIGNED,
 	
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER tweet_created
+	BEFORE INSERT ON tweets 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE images (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
-	author_id INTEGER(64) UNSIGNED,
+	author INTEGER(64) UNSIGNED,
 	file_name VARCHAR(32),
 	
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER image_created
+	BEFORE INSERT ON images 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE subscribers (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	subscriber_id INTEGER(64) UNSIGNED,
 	issuer_id INTEGER(64) UNSIGNED,
 	
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 )  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER subscriber_created
+	BEFORE INSERT ON subscribers 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE hashtags (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	
 	text VARCHAR(280),
 	
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER hashtag_created
+	BEFORE INSERT ON hashtags 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
 
 CREATE TABLE hashtag_appearances (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
-	tweet_id INTEGER(64) UNSIGNED,
-	hashtag_id INTEGER(64) UNSIGNED,
+	tweet INTEGER(64) UNSIGNED,
+	hashtag INTEGER(64) UNSIGNED,
 
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 
 CREATE TABLE notifications (
 	id INTEGER(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
-	user_id INTEGER(64),
-	read INTEGER(1) DEFAULT 0,
+	user INTEGER(64),
+	`read` INTEGER(1) DEFAULT 0,
 	text TEXT,
 	
-	created INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
-	modified INTEGER(64) DEFAULT UNIX_TIMESTAMP(CURRENT_TIMESTAMP),
+	created INTEGER(64),
+	modified INTEGER(64),
 	active INTEGER(1) DEFAULT 1
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TRIGGER notification_created
+	BEFORE INSERT ON notifications 
+	FOR EACH ROW 
+	SET 
+		new.created = UNIX_TIMESTAMP(NOW()),
+		new.modified = UNIX_TIMESTAMP(NOW());
